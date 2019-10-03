96e1be3ea691edd4fd0647039a2442272829217e
ccosgun@tspb.org.tr <ccosgun@tspb.org.tr>
2019-10-03 14:59:49 +0300

    no comment

--- OkulApp/OkulApp-ejb/src/main/java/com/okulapp/expopush/ExpoPushNotificationUtil.java
+++ OkulApp/OkulApp-ejb/src/main/java/com/okulapp/expopush/ExpoPushNotificationUtil.java
@@ -121,7 +121,7 @@ public class ExpoPushNotificationUtil {
     private static void sendToExpoServer(List<String> receivers, String title, String body) {
         List<String> datas = new ArrayList();
         for (String receiver : receivers) {
-            datas.add(new BasicDBObject("to", receiver).append("title", title).append("body", body).append("icon", "http://app.bilgiyuvamanaokulu.com/bilgiyuvam/images/icon.png").toJson());
+            datas.add(new BasicDBObject("to", receiver).append("title", title).append("body", body).toJson());
         }
         String data = datas.toString();
         try {
